0ff4792e19165ee92acc7bf9a281989f4200579c
Josua Blejeru <josua@blejeru.com>
2026-09-12 22:53:05 +0200

    set tab config

--- init.lua
+++ init.lua
@@ -112,6 +112,13 @@ do
   --  Experiment for yourself to see if you like it!
   vim.o.relativenumber = true
 
+  -- set vim tab config
+  -- tab to space (4x)
+  vim.o.tabstop = 4
+  vim.o.expandtab = true
+  vim.o.softtabstop = 4
+  vim.o.shiftwidth = 4
+
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
 
